62ab9ac529bf3f07367a6270cfba6c7f6bff95d5
sen <sen.take119@gmail.com>
2025-02-22 16:46:46 +0900

    更新 pxt.json, main.blocks, kokarobocon.ts

--- kokarobocon.ts
+++ kokarobocon.ts
@@ -3,19 +3,46 @@
 * このファイルを使って、独自の関数やブロックを定義してください。
 * 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
 */
-const course = {
-    前: '前',
-    後ろ: '後ろ',
-    右: '右',
-    左: '左',
-    止まる: '止まる'
+enum MyEnue {
+    //% block="前"
+    前,
+    //% block="後ろ"
+    後ろ,
+    //% block="右"
+    右,
+    //% block="左"
+    左,
+    //% block="止まる"
+    止まる,
 };
-
+let speed: number
 
 /**
  * Custom blocks
  */
 //% weight=100 color=#0fbc11 icon=""
-namespace こうかにんじゃロボコン {
-
+namespace ロボコン {
+    //% block
+    export function 回転サーボ(speed: number, e: MyEnue): void {
+        if (MyEnue.前 == MyEnue.前){
+        servos.P1.run(-speed)
+        servos.P2.run(speed)
+    }
+        if (MyEnue.後ろ == MyEnue.後ろ) {
+        servos.P1.run(speed)
+        servos.P2.run(-speed)
+    }
+        if (MyEnue.右 == MyEnue.右) {
+        servos.P1.stop()
+        servos.P2.run(speed)
+    }
+        if (MyEnue.左 == MyEnue.左) {
+        servos.P1.run(-speed)
+        servos.P2.stop()
+    }
+        if (MyEnue.止まる == MyEnue.止まる) {
+        servos.P1.stop()
+        servos.P2.stop()
+    }
+    }
 }
