3de4cddceae82b8f69ea1c6a9391bc575ae464b0
sen <sen.take119@gmail.com>
2025-02-22 15:27:38 +0900

    更新 pxt.json, main.blocks, main.ts, kokarobocon.ts

---
+++ kokarobocon.ts
@@ -0,0 +1,21 @@
+
+/**
+* このファイルを使って、独自の関数やブロックを定義してください。
+* 詳しくはこちらを参照してください：https://makecode.microbit.org/blocks/custom
+*/
+const course = {
+    前: '前',
+    後ろ: '後ろ',
+    右: '右',
+    左: '左',
+    止まる: '止まる'
+};
+
+
+/**
+ * Custom blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace こうかにんじゃロボコン {
+
+}
